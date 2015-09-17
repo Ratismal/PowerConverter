1640f44b56a67639a6c55b42337b66c6db51bc40
ratismal <kirkbenm@gmail.com>
2015-09-17 12:13:51 -0600

    Finished particles on plunder, added debug logger

---
+++ src/main/java/io/github/ratismal/felineutilities/util/Broadcaster.java
@@ -0,0 +1,28 @@
+package io.github.ratismal.felineutilities.util;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ratismal on 2015-09-17.
+ */
+
+public class Broadcaster {
+
+    public static void broadcastMessage(String msg) {
+
+        List<EntityPlayerMP> list = new ArrayList<EntityPlayerMP>();
+        list = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+        for (EntityPlayerMP player : list) {
+
+            player.addChatMessage(new ChatComponentText(msg));
+
+        }
+
+    }
+
+}
